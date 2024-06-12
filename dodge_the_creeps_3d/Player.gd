2e13a15ec8aa0465641c27bd0f45adda2c323990
karpetoryba <karpenkokarpenko24@gmail.com>
2024-06-12 11:19:42 +0200

    new flag

--- dodge_the_creeps_3d/Player.gd
+++ dodge_the_creeps_3d/Player.gd
@@ -1,7 +1,5 @@
 extends KinematicBody
-
 signal hit
-
 # How fast the player moves in meters per second.
 export var speed = 14
 # Vertical impulse applied to the character upon jumping in meters per second.
@@ -10,10 +8,7 @@ export var jump_impulse = 20
 export var bounce_impulse = 16
 # The downward acceleration when in the air, in meters per second per second.
 export var fall_acceleration = 75
-
 var velocity = Vector3.ZERO
-
-
 func _physics_process(delta):
 	var direction = Vector3.ZERO
 	if Input.is_action_pressed("move_right"):
@@ -24,7 +19,6 @@ func _physics_process(delta):
 		direction.z += 1
 	if Input.is_action_pressed("move_forward"):
 		direction.z -= 1
-
 	if direction != Vector3.ZERO:
 		# In the lines below, we turn the character when moving and make the animation play faster.
 		direction = direction.normalized()
@@ -32,20 +26,16 @@ func _physics_process(delta):
 		$AnimationPlayer.playback_speed = 4
 	else:
 		$AnimationPlayer.playback_speed = 1
-
 	velocity.x = direction.x * speed
 	velocity.z = direction.z * speed
-
 	# Jumping.
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y += jump_impulse
-
 	# We apply gravity every frame so the character always collides with the ground when moving.
 	# This is necessary for the is_on_floor() function to work as a body can always detect
 	# the floor, walls, etc. when a collision happens the same frame.
 	velocity.y -= fall_acceleration * delta
 	velocity = move_and_slide(velocity, Vector3.UP)
-
 	# Here, we check if we landed on top of a mob and if so, we kill it and bounce.
 	# With move_and_slide(), Godot makes the body move sometimes multiple times in a row to
 	# smooth out the character's motion. So we have to loop over all collisions that may have
@@ -58,15 +48,13 @@ func _physics_process(delta):
 			if Vector3.UP.dot(collision.normal) > 0.1:
 				mob.squash()
 				velocity.y = bounce_impulse
-
 	# This makes the character follow a nice arc when jumping
 	$Pivot.rotation.x = PI / 6 * velocity.y / jump_impulse
-
-
+func _ready():
+	# Connexion du signal de collision
+	$Area.connect("body_entered", self, "_on_Player_body_entered")
 func die():
 	emit_signal("hit")
 	queue_free()
-
-
-func _on_MobDetector_body_entered(_body):
+func _on_Player_body_entered(_body):
 	die()
